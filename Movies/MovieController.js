0f43897f21b3babc97f95c78c9722e465573138b
Raul <raaul9212@gmail.com>
2021-10-28 19:09:40 +0200

    Funcion de añadir pelicula

--- Movies/MovieController.js
+++ Movies/MovieController.js
@@ -2,8 +2,23 @@
 
 const model=require("./MovieModels")
 
+//Añadir peliculas en el body a la BBDD
+
 module.exports.createMovie = async (req,res)=>{
     const movie = new model(req.body);
     await movie.save();
     res.json(movie)
+}
+
+//Buscar por titulo e id
+
+module.exports.SearchMovie = async (req,res)=>{
+    const Search=(films)=>{
+        let search =[];
+        for (let i = 0; i < movieArr.length; i++){
+            if(model.movieArr.includes(films)){
+                
+            }
+        }
+    }
 }
--- Movies/MovieRouter.js
+++ Movies/MovieRouter.js
@@ -5,26 +5,10 @@ const controller = require("./MovieController");
 
 // Buscar pelicula por ID
 
-/*
-router.get("/movie",(req,res)=>{
-    const SearchName=(name)=>{
-        let search=[];
-        for (let i = 0; i < movieArr.length; i++) {
-            if(movieArr[i].includes(name)){
-                search.push(movieArr[i]);
-                console.log(i);
-            }
-            
-        }
-        console.log(search)
-        return search;
-    }
-    if(req.query.find){res.json({search:SearchName(req.query.find)})}
-    else{res.json({All: movieArr[req.params.id]})}
-})*/
+
 
 router.get("/",(req,res)=>res.json({All: movieArr[req.params.id]})) // Buscar por Id
 
-router.post("/",controller.createMovie)
+router.post("/",controller.createMovie) // Ruta para añadir peliculas en el boy a la BBDD
 
 module.exports = router;
