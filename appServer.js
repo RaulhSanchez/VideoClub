d8e88f341b76c48cf76cfb3b1e2978f3545951e8
Raul <raaul9212@gmail.com>
2021-10-28 16:21:30 +0200

    Estructura y servidor

---
+++ appServer.js
@@ -0,0 +1,7 @@
+const { Router } = require("express");
+const express = require("express");
+
+//app.use("/user"); // Acceso a carpeta user
+
+// Iniciar servidor
+app.listen(3000,()=>console.log("Servidor levantado en el puerto 3000"));
