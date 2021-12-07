7c084f2ec01ee8bfe5c8647ec2bcf85611c1a57e
Raul <raaul9212@gmail.com>
2021-12-07 22:21:29 +0100

    Last version

--- Movies/MovieController.js
+++ Movies/MovieController.js
@@ -22,17 +22,25 @@ module.exports.CreateMovie = async (req,res)=>{
 //Peliculas por titulo
 
 module.exports.SearchMovieByTitle = async (req,res)=>{
-    const query={};
-    if(req.query.title)query.title = req.query.title;
-    if(req.query.director)query.director = req.query.director;
-    if(req.query.gender)query.gender = req.query.gender;
-    const rest = await model.find(query);
-    res.json(rest);
+    try{
+        const query={};
+        if(req.query.title)query.title = req.query.title;
+        if(req.query.director)query.director = req.query.director;
+        if(req.query.gender)query.gender = req.query.gender;
+        const rest = await model.find(query);
+        res.json(rest);
+    }catch(error){
+        res.status(400).json("No se ha encontrado ninguna película")
+    }
 }
 
 // BUsqueda por ID
 
 module.exports.SearchMovieById = async (req,res)=>{
+    try{
     const result = await MovieModels.find({_id: req.params.id})
     res.json(result);
+    }catch(error){
+        res.status(400).json("No se ha encontrado ninguna película")
+    }
 }
